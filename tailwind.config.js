ef6b4d66b247d508c6faf2eb28ccd097cf397e43
abyanmusyaffa <abyanaufaalifmusyaffa@gmail.com>
2024-06-13 20:20:26 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('./node_modules/tailwindcss/defaultTheme');
-const { addDynamicIconSelectors } = require('./node_modules/@iconify/tailwind');
+const defaultTheme = require("./node_modules/tailwindcss/defaultTheme");
+const { addDynamicIconSelectors } = require("./node_modules/@iconify/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,11 +11,9 @@ module.exports = {
         sans: ['"Baloo 2"', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
-        'mentari' : '4px 4px 10px rgba(228, 230, 232, 0.90) inset',
+        mentari: "4px 4px 10px rgba(228, 230, 232, 0.90) inset",
       },
     },
   },
-  plugins: [
-    addDynamicIconSelectors(),
-  ],
-}
+  plugins: [addDynamicIconSelectors()],
+};
